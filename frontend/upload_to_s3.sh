1ac177cd71532f4e04c0f4813076c33b68a19ba1
Ville Seppanen <ville.seppanen@gofore.com>
2015-10-25 15:44:25 +0200

    Do not upload local test data to s3 bucket

--- frontend/upload_to_s3.sh
+++ frontend/upload_to_s3.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-# This script uploads the current directory to S3 to be hosted as a static website
-
-aws s3 cp . s3://after.heap.fi/ --profile heap --region eu-west-1 --recursive --exclude ".git/*"
+# Uploads the current directory to S3 to be hosted as a static website
+aws s3 cp . s3://after.heap.fi/ --profile heap --region eu-west-1 --recursive --exclude "data/*"
