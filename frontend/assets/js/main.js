394623b5503266a09ce3e46881948c24ba67d19b
Ville Seppanen <ville.seppanen@gofore.com>
2015-12-09 19:09:33 +0200

    Rewrite using lodash (courtesy of baabelfish)

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,30 +1,29 @@
-var venueBlacklist = [
-  1782433, // As Oy Sorsapuisto
-  865959 // Stefan's Steakhouse
-];
+var venueBlacklist = {
+  1379610: "Amica-ravintola Valtatie 30",
+  1782433: "As Oy Sorsapuisto",
+  865959:  "Stefan's Steakhouse",
+};
 
-var beerBlacklist = [
-  36800, // Lapin Kulta Premium III
-  39534 // Karhu A
-];
+var beerBlacklist = {
+  39534: "Karhu A",
+  39540: "Karhu III",
+  36800: "Lapin Kulta Premium III",
+};
 
 angular.module('afteri', []).controller('afteriCtrl', ['$scope', '$http', function($scope, $http) {
 
   $http.get('data/thepub.json').then(function (response) {
-    var checkins = response.data.response.checkins.items;
-    console.log(checkins);
-    
-    /* Filter blacklisted venues and beers */
-    checkins = $.grep(checkins, function(item) {
-      return jQuery.inArray(item.venue.venue_id, venueBlacklist) == -1 && jQuery.inArray(item.beer.bid, beerBlacklist) == -1;
+    var checkins = _.reject(response.data.response.checkins.items, function(item) {
+      return venueBlacklist[item.venue.venue_id] || beerBlacklist[item.beer.bid];
     });
 
-    var uniqueVenues = {};
-    $.each(checkins, function(index, checkin){
-      uniqueVenues[checkin.venue.venue_id.toString()] = checkin;
-    });
+    var topSuggestions = _(checkins)
+      .unique('venue.venue_id')
+      .sortBy('beer.beer_abv')
+      .reverse()
+      .take(3)
+      .value();
 
-    var topSuggestions = $.map(uniqueVenues, function(venue) {return venue;}).slice(0, 3);
     $scope.suggestions = topSuggestions;
     $scope.checkins = checkins;
 
