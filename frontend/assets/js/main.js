155a6d78d0ec91a6b329756ee42e2f9efdf3484c
Ville Seppanen <ville.seppanen@gofore.com>
2016-01-16 20:15:43 +0200

    Update blacklists

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,14 +1,20 @@
 
 var venueBlacklist = {
+  547302:  "Ale Pupi",
   1379610: "Amica-ravintola Valtatie 30",
   1782433: "As Oy Sorsapuisto",
+  951950:  "Keskustori",
+  125270:  "Näsin sauna",
   865959:  "Stefan's Steakhouse",
+  1293012: "Tampere",
+  2566357: "TKL bussi 28",
 };
 
 var beerBlacklist = {
-  39534: "Karhu A",
-  39540: "Karhu III",
-  36800: "Lapin Kulta Premium III",
+  39534:  "Karhu A",
+  39540:  "Karhu III",
+  36800:  "Lapin Kulta Premium III",
+  925902: "Long Drink Grapefruit",
 };
 
 angular.module('afteri', []).controller('afteriCtrl', ['$scope', '$http', function($scope, $http) {
