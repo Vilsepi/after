8276c0f0802bf486d40ec96f93c7902dfa79a2aa
Ville Seppanen <ville.seppanen@gofore.com>
2016-01-16 23:20:16 +0200

    Remove unused scope variable

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -34,7 +34,6 @@ angular.module('afteri', []).controller('afteriCtrl', ['$scope', '$http', functi
       .value();
 
     $scope.suggestions = topSuggestions;
-    $scope.filteredCheckins = filteredCheckins;
     $scope.checkins = checkins;
 
   });
