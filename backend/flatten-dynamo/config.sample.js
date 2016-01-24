a77c1f62ee208b5935d4a8c56d79c744ea3142e9
Ville Seppanen <ville.seppanen@gofore.com>
2016-01-24 22:22:14 +0200

    Add initial implementation for caching beers and venues separately

---
+++ backend/flatten-dynamo/config.sample.js
@@ -0,0 +1,16 @@
+var areas = {
+  tampere:  {name: 'tampere',  lat: 61.4985, lng: 23.7717, radius: 1.06},
+  helsinki: {name: 'helsinki', lat: 60.1671, lng: 24.9409, radius: 1.49},
+};
+
+var config = {
+  remoteClientId: '',
+  remoteClientSecret: '',
+  remoteHost: '',
+  remotePath: '',
+  area: areas.tampere,
+  tableBeer: '',
+  tableVenue: ''
+};
+
+module.exports = config;
---
+++ backend/flatten-dynamo/index.js
@@ -0,0 +1,104 @@
+"use strict";
+
+var https = require('https');
+var qs = require('querystring');
+var _ = require('lodash');
+var doc = require('dynamodb-doc');
+var dynamo = new doc.DynamoDB();
+var config = require('./config');
+
+exports.handler = function(event, context) {
+
+  // Helper that removes keys which have empty string as value (DynamoDB does not allow them)
+  function deleteEmptyStrings(obj) {
+    for (var k in obj) {
+      if (obj[k] === '') {
+        delete obj[k];
+      } else if (typeof obj[k] === 'object') {
+        deleteEmptyStrings(obj[k]);
+      }
+    }
+  }
+
+  // Tries to batch write items until succeeds or lambda timeouts
+  function determinedBatchWrite(dynamoParams, batchDoneCallback) {
+    dynamo.batchWriteItem(dynamoParams, function(err, data) {
+      if (err) {
+        context.fail(err);
+      }
+      else {
+        if (_.isEmpty(data.UnprocessedItems)) {
+          batchDoneCallback();
+        }
+        else {
+          console.log("UnprocessedItems: " + _.find(data.UnprocessedItems).length);
+          var params = {};
+          params.RequestItems = data.UnprocessedItems;
+          determinedBatchWrite(params, batchDoneCallback);
+        }
+      }
+    });  
+  }
+
+  // Split checkins into beers and venues and write them to dynamo
+  function processCheckins(checkins) {
+    deleteEmptyStrings(checkins);
+
+    var beers = {};
+    var venues = {};
+    
+    checkins.forEach(function(checkin) {
+      beers[checkin.beer.bid] = checkin.beer;
+      venues[checkin.venue.venue_id] = checkin.venue;
+    });
+
+    // Helper that prepares hash items by wrapping them into dynamo requests
+    function itemsToParams(items, targetTable) {
+      var params = { RequestItems: {} };
+      params.RequestItems[targetTable] = _.map(_.values(items), function(item){
+        return { PutRequest: { Item: item } };
+      });
+      return params;
+    }
+
+    var beerParams = itemsToParams(beers, config.tableBeer);
+    var venueParams = itemsToParams(venues, config.tableVenue);
+
+    determinedBatchWrite(beerParams, determinedBatchWrite(venueParams, context.succeed()));
+  }
+
+  var httpQueryParams = {
+    client_id: config.remoteClientId,
+    client_secret: config.remoteClientSecret,
+    lat: config.area.lat,
+    lng: config.area.lng,
+    radius: config.area.radius
+  };
+
+  var httpOptions = {
+    host: config.remoteHost,
+    path: config.remotePath + '?' + qs.stringify(httpQueryParams)
+  };
+
+  https.get(httpOptions, function(res) {
+    console.log('Remote server rate limit remaining: ', res.headers['x-ratelimit-remaining']);
+    if (res.statusCode != 200) {
+      console.log('Received non-ok status code from remote: ', res.statusCode);
+      context.fail('Request to remote failed');
+    }
+
+    var responseBody = '';
+    res.on('data', function(chunk) { responseBody += chunk; });
+    res.on('end', function() {
+      console.log('Received response, remaining millis ' + context.getRemainingTimeInMillis());
+
+      var checkins = JSON.parse(responseBody).response.checkins.items;
+      processCheckins(checkins);
+    });
+
+  }).on('error', function(e) {
+    console.log(e.message);
+    context.fail('Failed to get response from remote');
+  });
+
+};
