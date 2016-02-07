e2287eb59ac1637ad4645cbde87bb1600c316b5a
Ville Seppanen <ville.seppanen@gofore.com>
2016-02-07 22:54:29 +0200

    Remove unused moment

--- frontend/src/app/index.constants.js
+++ frontend/src/app/index.constants.js
@@ -1,11 +1,9 @@
-/* global moment:false */
 /* global _:false */
 (function() {
   'use strict';
 
   angular
     .module('afterHeap')
-    .constant('moment', moment)
     .constant('_', _);
 
 })();
