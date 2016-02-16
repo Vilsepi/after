4dcd768130716ce450af7f8b09ee11d4aa0372de
Ville Seppanen <ville.seppanen@gofore.com>
2016-02-16 22:16:21 +0200

    Fix bower installation

--- travis/frontend-install.sh
+++ travis/frontend-install.sh
@@ -2,5 +2,11 @@
 
 cd frontend
 
+# Install global tools
+npm install -g bower
+
+# Install npm dependencies from package.json
 npm install
+
+# Install bower dependencies from bower.json
 bower install
