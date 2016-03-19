a14cce5999a300bc3ce68ec9311f1240fb60a779
Ville Seppanen <ville.seppanen@gofore.com>
2016-03-19 16:46:46 +0200

    Remove about and dataType views

--- frontend-angular2/lib/components/index.ts
+++ frontend-angular2/lib/components/index.ts
@@ -1,10 +1,8 @@
 import {Component}          from 'angular2/core';
 import {Navbar}             from './navbar/index';
 import {Home}               from './home/index';
-import {About}              from './about/index';
 import {GithubUsers}        from './githubUsers/index';
 import {Activity}           from './activity/index';
-import {DataType}           from './dataType/index';
 import {RouteConfig, Route, ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
@@ -15,9 +13,7 @@ import {RouteConfig, Route, ROUTER_DIRECTIVES} from 'angular2/router';
 })
 @RouteConfig([
     new Route({ path: '/', component: Home, name: 'Home' }),
-    new Route({ path: '/about', component: About, name: 'About', data: { pageId: null } }),
     new Route({ path: '/activity', component: Activity, name: 'Activity' }),
-    new Route({ path: '/datatype', component: DataType, name: 'DataType' }),
     new Route({ path: '/github-users/...', component: GithubUsers, name: 'GithubUsers'})
 ])
 export class App {
