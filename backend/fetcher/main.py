ca97eb610d2ba163a894905d1178446a253a7b73
Ville Seppanen <ville.seppanen@gofore.com>
2016-06-05 19:32:22 +0300

    Write beers and venues to simpledb

--- backend/fetcher/main.py
+++ backend/fetcher/main.py
@@ -3,26 +3,29 @@
 import boto3
 import requests
 import json
+import datetime
 from collections import defaultdict
 import config
 
 s3 = boto3.resource('s3')
+simpledb = boto3.client('sdb')
 
 # Fetches latest checkins from a given geographical area
-def get_checkins(area):
+def get_checkins(area_id):
+    print "Fetching latest checkins from remote for area " + area_id
     url = config.remote['base_url'] + '/thepub/local'
     params = {
         'client_id': config.remote['client_id'],
         'client_secret': config.remote['client_secret'],
-        'lat': area['lat'],
-        'lng': area['lng'],
-        'radius': area['radius']
+        'lat': config.areas[area_id]['lat'],
+        'lng': config.areas[area_id]['lng'],
+        'radius': config.areas[area_id]['radius']
     }
-    result = requests.get(url, params=params)
+    response = requests.get(url, params=params)
+    print "Ratelimit remaining:", response.headers.get('x-ratelimit-remaining')
 
-    if result.status_code == 200:
-        print "Received OK response from remote"
-        return result.json().get('response').get('checkins').get('items')
+    if response.status_code == 200:
+        return response.json().get('response').get('checkins').get('items')
     return []
 
 # Removes uninteresting keys from data
@@ -32,12 +35,47 @@ def filter_checkins(checkins):
         [checkin.get('user').pop(key, None) for key in ['is_private', 'is_supporter', 'relationship', 'bio', 'website']]
         [checkin.get('beer').pop(key, None) for key in ['beer_slug', 'beer_description', 'is_in_production', 'auth_rating', 'wish_list', 'beer_active']]
         [checkin.get('brewery').pop(key, None) for key in ['brewery_slug', 'brewery_active']]
-        [checkin.get('venue').pop(key, None) for key in ['venue_slug', 'is_verified']]
+
+        if checkin.get('venue'):
+            [checkin.get('venue').pop(key, None) for key in ['venue_slug', 'is_verified']]
+            [checkin.get('venue').get('venue_icon').pop(key, None) for key in ['sm', 'md']]
+            [checkin.get('venue').get('location').pop(key, None) for key in ['venue_country']]
+            [checkin.get('venue').get('contact').pop(key, None) for key in ['twitter']]
+
     return checkins
 
-# Saves checkins, venues and beers to DynamoDB
-def save_all_to_dynamodb(checkins, area_id):
-    pass
+# Writes items to SimpleDB
+def simpledb_put(items, domain, area_id):
+    for item_id, item in items.iteritems():
+        json_object = json.dumps(item)
+        response = simpledb.put_attributes(
+            DomainName=domain,
+            ItemName=str(item_id),
+            Attributes=[
+                {
+                    'Name': 'json_object',
+                    'Value': json_object,
+                    'Replace': True
+                },
+                {
+                    'Name': 'json_length',
+                    'Value': str(len(json_object)).zfill(5),
+                    'Replace': True
+                },
+                {
+                    'Name': 'json_updated',
+                    'Value': datetime.datetime.utcnow().isoformat() + 'Z',
+                    'Replace': True
+                },
+                {
+                    'Name': 'area',
+                    'Value': str(area_id),
+                    'Replace': True
+                }
+            ]
+        )
+        if response['ResponseMetadata']['HTTPStatusCode'] != 200:
+            print "Error while storing object to SimpleDB:", response
 
 # Saves checkins to S3 as JSON
 def save_checkins_to_s3(checkins, area_id):
@@ -50,8 +88,7 @@ def save_checkins_to_s3(checkins, area_id):
 # Lambda handler, fetches, cleans and stores checkin data
 def lambda_handler(event, context):
     for area_id, area in config.areas.iteritems():
-        print "Fetching latest checkins from remote for area " + area_id
-        checkins = filter_checkins(get_checkins(config.areas[area_id]))
+        checkins = filter_checkins(get_checkins(area_id))
         venues = defaultdict(dict)
         beers = defaultdict(dict)
 
@@ -61,6 +98,9 @@ def lambda_handler(event, context):
             beers[checkin.get('beer').get('bid')] = checkin.get('beer')
 
         save_checkins_to_s3(checkins, area_id)
+
+        simpledb_put(beers, config.storage['table_beer'], area_id)
+        simpledb_put(venues, config.storage['table_venue'], area_id)
 
 
 if __name__ == '__main__':
