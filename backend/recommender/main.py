66be654a7fd974abff010c5ea115119ed544fd39
Ville Seppanen <ville.seppanen@gofore.com>
2016-07-25 01:13:14 +0300

    Fix bug where recommendation count would not increase

--- backend/recommender/main.py
+++ backend/recommender/main.py
@@ -46,18 +46,6 @@ def pick_recommended_venues(area):
     print "Now we have {} and we should get {}".format(len(recommended_venues), MAX_COUNT)
     return recommended_venues
 
-# Deserializes JSON stored in SimpleDB and flattens dictionaries
-def flatten_structures(simpledb_items):
-    venues = []
-    for item in simpledb_items:
-        venue = defaultdict(dict)
-        attributes = {attr['Name']:attr['Value'] for attr in item['Attributes']}
-
-        venue = json.loads(attributes['json_object'])
-
-        venues.append(venue)
-    return venues
-
 # Writes Python structure to S3 as JSON
 def write_to_s3(key, data):
     response = s3.Bucket(config.storage['bucket_name']).put_object(
@@ -82,25 +70,25 @@ def get_detailed_venue(venue_id):
     return []
 
 # Updates recommendations
-def update_recommendations(venues, area):
+def update_recommendations(venue_sdb_items, area):
     recommendations_updated = datetime.datetime.utcnow().isoformat() + 'Z'
-
-    # TODO venue objects are actually completely unnecessary. To fetch the top
-    # beers of a venue, the detailed venue_info must be fetched, which contains
-    # everything and more than the basic venue objects.
     detailed_venues = []
-    for venue in venues:
-        venue['recommendation_count'] = int(venue.get('recommendation_count', '0')) + 1
-        detailed_venues.append(get_detailed_venue(venue['venue_id']))
+    for venue_sdb_item in venue_sdb_items:
+        attributes = {attr['Name']:attr['Value'] for attr in venue_sdb_item['Attributes']}
+        venue_id = venue_sdb_item['Name']
+
+        attributes['recommendation_count'] = int(attributes.get('recommendation_count', '0')) + 1
+
+        detailed_venues.append(get_detailed_venue(venue_id))
 
         # Update recommendation count and timestamp in SimpleDB
         response = simpledb.put_attributes(
             DomainName=config.storage['table_venue'],
-            ItemName=str(venue['venue_id']),
+            ItemName=venue_id,
             Attributes=[
                 {
                     'Name': 'recommendation_count',
-                    'Value': str(venue['recommendation_count']).zfill(7),
+                    'Value': str(attributes['recommendation_count']).zfill(7),
                     'Replace': True
                 },
                 {
@@ -120,8 +108,8 @@ def update_recommendations(venues, area):
 
 def lambda_handler(event, context):
     for area in config.areas:
-        venues = flatten_structures(pick_recommended_venues(area))
-        update_recommendations(venues, area)
+        venue_sdb_items = pick_recommended_venues(area)
+        update_recommendations(venue_sdb_items, area)
 
 
 if __name__ == '__main__':
