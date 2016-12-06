e1ef3c68aea83422ef825b9de32cb57772bb79c1
Ville Seppanen <ville.seppanen@gofore.com>
2016-12-06 16:57:22 +0200

    Add tool to deploy gzipped js bundles

--- frontend/upload_to_s3.sh
+++ frontend/upload_to_s3.sh
@@ -9,18 +9,24 @@ REGION="eu-west-1"
 PROFILE="heap"
 BUCKET="after.heap.fi"
 
-SYNC_BASE_ARGS="s3://$BUCKET/ --profile $PROFILE --region $REGION"
-SYNC_FILE_ARGS="--exclude '*.map*' --exclude '*.gz'"
+PROFILE_ARGS="--profile $PROFILE --region $REGION"
+SYNC_FILE_ARGS="s3://$BUCKET/ --exclude '*.map*' --exclude '*.gz'"
 
 if [ $# -lt 1 ]
 then
   echo "Simulating S3 sync, no changes are made"
-  eval aws s3 sync dist $SYNC_BASE_ARGS $SYNC_FILE_ARGS --dryrun
+  eval aws s3 sync dist $SYNC_FILE_ARGS $PROFILE_ARGS --dryrun
   echo "To deploy, re-execute with --doit"
 else
   if [ $1 == "--doit" ]; then
     echo "Uploading assets"
-    eval aws s3 sync dist $SYNC_BASE_ARGS $SYNC_FILE_ARGS
+    eval aws s3 sync dist $SYNC_FILE_ARGS $PROFILE_ARGS
+  elif [ $1 == "--dozipped" ]; then
+    echo "Uploading zipped assets"
+    eval aws s3 cp dist/index.html s3://$BUCKET/index.html $PROFILE_ARGS
+    eval aws s3 cp dist/error.html s3://$BUCKET/error.html $PROFILE_ARGS
+    eval aws s3 cp dist/bundle.js.gz s3://$BUCKET/bundle.js --content-encoding gzip $PROFILE_ARGS
+    eval aws s3 cp dist/vendor.bundle.js.gz s3://$BUCKET/vendor.bundle.js --content-encoding gzip $PROFILE_ARGS
   else
     echo "Unknown arguments. Either use no args to dryrun or use --doit to upload"
   fi
